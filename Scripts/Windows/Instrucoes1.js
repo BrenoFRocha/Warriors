ce36662d54ad5a14f757e52cf87c3ed9eceece20
Breno Rocha <brenoferreirarocha@gmail.com>
2019-01-05 02:15:13 -0200

    Adiciona primeira tela de instruções e altera botão de voltar e avançar

--- Scripts/Windows/Instrucoes1.js
+++ Scripts/Windows/Instrucoes1.js
@@ -1,32 +1,37 @@
-var Instru1 = new Image();
-Instru1.src = "Images/intrucoes.png";
-
-var setinha = new Image();
-setinha.src = "Images/avancar.png";
+var I_background_image = new Image();
 
 
+var I_next_image = new Image();
+I_next_image.src = "Images/next.png";
+
+var I_exit_button = {x:72.5, y:80, w: 64, h: 64};
+var I_next_button = {x:667.5, y:475, w:64, h:64};
 
 function SetInstru1()
 {
-	context.drawImage(Instru1,0,0,canvas.width,canvas.height);
+	if(PTBR)
+	{
+		I_background_image.src = "Images/Instructions_1_Background.png";
+	}
+	else
+	{
+		I_background_image.src = "Images/Instrucoes_1_Background.png";
+	}
+	context.drawImage(I_background_image,0,0);
 	if(fadeOut)
 	{
 		fadeIn = true;
 		fadeOut = false;
 	}
 	
-	
-	var bottonI = {x:canvas.width - 200, y:50, w:150, h:75};
-	var bottonM = {x:canvas.width - 200, y:canvas.height - 100, w:150, h:50};
-	
-	context.drawImage(setinha,bottonI.x,bottonI.y,bottonI.w,bottonI.h);
+	context.drawImage(I_next_image,I_next_button.x,I_next_button.y,I_next_button.w,I_next_button.h);
 
-	context.drawImage(exit_button_image,bottonM.x,bottonM.y,bottonM.w,bottonM.h);
+	context.drawImage(exit_button_image,I_exit_button.x,I_exit_button.y,I_exit_button.w,I_exit_button.h);
 	
-	if(mouse.x >= bottonI.x &&
-	mouse.x <= bottonI.x + bottonI.w &&
-	mouse.y >= bottonI.y &&
-	mouse.y <= bottonI.y + bottonI.h &&
+	if(mouse.x >= I_next_button.x &&
+	mouse.x <= I_next_button.x + I_next_button.w &&
+	mouse.y >= I_next_button.y &&
+	mouse.y <= I_next_button.y + I_next_button.h &&
 	mouse.press)
 	{
 		fadeOut = true;
@@ -34,10 +39,10 @@ function SetInstru1()
 		auxScene = "Instrucoes8";
 	}
 	
-	if(mouse.x >= bottonM.x &&
-	mouse.x <= bottonM.x + bottonM.w &&
-	mouse.y >= bottonM.y &&
-	mouse.y <= bottonM.y + bottonM.h &&
+	if(mouse.x >= I_exit_button.x &&
+	mouse.x <= I_exit_button.x + I_exit_button.w &&
+	mouse.y >= I_exit_button.y &&
+	mouse.y <= I_exit_button.y + I_exit_button.h &&
 	mouse.press)
 	{
 		fadeOut = true;
